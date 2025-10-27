c01eb7a2249fce56dd05c9105fb1ea0a7593505e
AndreySparkle <adubravin4445@gmail.com>
2025-10-27 21:09:54 +0200

    clear

--- Python_Back/app/api/endpoints/__init__.py
+++
@@ -1,4 +0,0 @@
-from app.api.endpoints.schemas import router as schemas_router
-from app.api.endpoints.templates import router as templates_router
-
-__all__ = ["schemas_router", "templates_router"]
--- Python_Back/app/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from app.models.models import ServiceSchema, Template
-
-__all__ = ["ServiceSchema", "Template"]
